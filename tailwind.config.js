36cc01384ae60a65c708252d0b13eb0bb99484cd
Tiffany M <tm250604@gmail.com>
2025-12-29 16:58:17 -0500

    add initial login page, routing, and integrate HeroUI with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const { heroui } = require("@heroui/react")
+
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    heroui({
+      prefix: "heroui",
+      addCommonColors: false, 
+      defaultTheme: "light", 
+      defaultExtendTheme: "light",
+    }),
+  ],
+}
