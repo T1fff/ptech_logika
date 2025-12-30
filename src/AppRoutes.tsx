ffd988c17cda9989ed92a12ba420242bd7c05f9c
Tiffany M <tm250604@gmail.com>
2025-12-30 17:53:49 -0500

    feat: implement private route for dashboard and update routing structure for improved navigation

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router';
+import { Navigate, Outlet, Route, Routes } from 'react-router';
 import Login from '@pages/Login/Login';
 import Home from './pages/Dashboard/Home';
 import SocialImpact from './pages/Dashboard/SocialImpact';
@@ -7,21 +7,46 @@ import Sponsors from './pages/Dashboard/Sponsors';
 import Categories from './pages/Dashboard/Categories';
 import Contents from './pages/Dashboard/Contents';
 import Marketplace from './pages/Dashboard/Marketplace';
+import { useAuth } from '@/hooks/useAuth';
+
+import type { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+const PrivateRoute: React.FC<Props> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
+};
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard/home" element={<Home />} />
-      <Route path="/dashboard/impacto-social" element={<SocialImpact />} />
-      <Route path="/dashboard/community" element={<Community />} />
-      <Route path="/dashboard/sponsors" element={<Sponsors />} />
-      <Route path="/dashboard/marketplace" element={<Marketplace />} />
-      <Route path="/dashboard/contents" element={<Contents />} />
-      <Route path="/dashboard/categories" element={<Categories />} />
 
-      <Route path="*" element={<div>404 Not Found</div>} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Outlet />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<Navigate to="home" replace />} />
+
+        <Route path="home" element={<Home />} />
+        <Route path="impacto-social" element={<SocialImpact />} />
+        <Route path="community" element={<Community />} />
+        <Route path="sponsors" element={<Sponsors />} />
+        <Route path="marketplace" element={<Marketplace />} />
+        <Route path="contents" element={<Contents />} />
+        <Route path="categories" element={<Categories />} />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
